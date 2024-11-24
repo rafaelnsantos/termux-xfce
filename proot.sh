4f392ba43a778ced3459b218ac7fb525808a4965
Rafael <rafaelnsantos1@gmail.com>
2024-11-24 12:21:25 -0300

    fix

--- proot.sh
+++ proot.sh
@@ -70,26 +70,24 @@ pd login debian --shared-tmp -- env DISPLAY=:1.0 rm /etc/localtime
 pd login debian --shared-tmp -- env DISPLAY=:1.0 cp /usr/share/zoneinfo/$timezone /etc/localtime
 
 #Setup Fancybash Proot
-cp .fancybash.sh $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username
+cp fancybash.sh $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fancybash.sh
 echo "source ~/.fancybash.sh" >> $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.bashrc
 sed -i '327s/termux/proot/' $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fancybash.sh
 
 mkdir $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.config
-mv .config/neofetch $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.config
+mv config/neofetch $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.config
 
-#Set theming from xfce to proot
-cp -r $PREFIX/share/icons/dist-dark $PREFIX/var/lib/proot-distro/installed-rootfs/debian/usr/share/icons/dist-dark
 
 cat <<'EOF' > $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.Xresources
 Xcursor.theme: dist-dark
 EOF
 
 mkdir $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fonts/
-cp .fonts/NotoColorEmoji-Regular.ttf $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fonts/ 
+cp fonts/NotoColorEmoji-Regular.ttf $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username/.fonts/ 
 
 
 #Setup Hardware Acceleration
-mv files/mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb $PREFIX/var/lib/proot-distro/installed-rootfs/debian/home/$username
+mv files/mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb $PREFIX/var/lib/proot-distro/installed-rootfs/debian/root
 pd login debian --shared-tmp -- env DISPLAY=:1.0 sudo apt install -y ./mesa-vulkan-kgsl_24.1.0-devel-20240120_arm64.deb
 
 
